19f499a71b629535a6dbedbc03e690f88bf6062b
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-21 05:50:44 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ge

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {geminiPro} from 'genkitx-googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: geminiPro,
+  model: 'googleai/gemini-pro',
 });
