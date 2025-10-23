49c1ed0db246182095dbaa987f72ed34ca6cf3ec
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-23 17:41:55 +0000

    ok continue

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -1,6 +1,5 @@
 
 
-
 'use client';
 import { Suspense } from 'react';
 import Image from 'next/image';
--- src/app/student/tp/[tpId]/page.tsx
+++ src/app/student/tp/[tpId]/page.tsx
@@ -1,6 +1,5 @@
 
 
-
 'use client';
 import { useState } from 'react';
 import { useParams, useSearchParams } from 'next/navigation';
