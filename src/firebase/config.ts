2f0698d7ca12b19458f100b70331973bad59df97
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-23 18:22:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9087751774-c7d8c",
+  "appId": "1:205810415867:web:3331939a9bb7725571985e",
+  "apiKey": "AIzaSyBibbsL7BvzFsqU7uz3U-I6iZcUl7IVH8I",
+  "authDomain": "studio-9087751774-c7d8c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "205810415867"
+};
