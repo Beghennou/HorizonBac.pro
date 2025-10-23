395560db9e756c5dbd44d50decb8e10b078d6f3d
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-23 10:04:51 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBibbsL7BvzFsqU7uz3U-I6iZcUl7IV

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBibbsL7BvzFsqU7uz3U-I6iZcUl7IVH8I",
   authDomain: "studio-9087751774-c7d8c.firebaseapp.com",
   projectId: "studio-9087751774-c7d8c",
-  storageBucket: "studio-9087751774-c7d8c.appspot.com",
+  storageBucket: "studio-9087751774-c7d8c.firebasestorage.app",
   messagingSenderId: "205810415867",
   appId: "1:205810415867:web:48776186c118ddf971985e"
 };
