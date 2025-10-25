471f2ab5ad58b3a301220a8c531ec0e13d5b2529
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-25 13:43:40 +0000

    annul et reviens comme avant

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,8 @@ export const firebaseConfig = {
   apiKey: "AIzaSyC7TFUBsU5cyQCj3F1I4TRLR0z6uU69_lw",
   authDomain: "studio-9087751774-c7d8c.firebaseapp.com",
   projectId: "studio-9087751774-c7d8c",
-  storageBucket: "studio-9087751774-c7d8c.appspot.com",
+  storageBucket: "studio-908775174-c7d8c.appspot.com",
   messagingSenderId: "205810415867",
   appId: "1:205810415867:web:8c57ab38250c33a971985e"
 };
+
