ae58e14ffbb8285bfb7caddeaafa43369eb68c17
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-25 13:41:33 +0000

    {   "project_info": {     "project_number": "205810415867",     "project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBibbsL7BvzFsqU7uz3U-I6iZcUl7IVH8I",
-  authDomain: "studio-9087751774.firebaseapp.com",
-  projectId: "studio-9087751774",
-  storageBucket: "studio-9087751774.appspot.com",
+  apiKey: "AIzaSyC7TFUBsU5cyQCj3F1I4TRLR0z6uU69_lw",
+  authDomain: "studio-9087751774-c7d8c.firebaseapp.com",
+  projectId: "studio-9087751774-c7d8c",
+  storageBucket: "studio-9087751774-c7d8c.appspot.com",
   messagingSenderId: "205810415867",
   appId: "1:205810415867:web:8c57ab38250c33a971985e"
 };
