2a18a4518250bc627da7b5c36c24bb5fcf51709f
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-25 21:46:56 +0000

    je veuD'accord, merci beaucoup pour le contenu complet du fichier src/ap

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState, useEffect } from 'react';
@@ -59,7 +60,7 @@ export function useCollection<T = any>(
   type StateDataType = ResultItemType[] | null;
 
   const [data, setData] = useState<StateDataType>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<FirestoreError | Error | null>(null);
 
   useEffect(() => {
@@ -121,3 +122,4 @@ export function useCollection<T = any>(
   }
   return { data, isLoading, error };
 }
+
