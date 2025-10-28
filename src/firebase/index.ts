46202c086b4eff1b68bde862be6bfab3b9adbd76
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-28 21:43:19 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 'use client';
 
 import * as React from 'react';
@@ -42,7 +43,7 @@ export function getSdks(firebaseApp: FirebaseApp) {
 }
 
 export * from './provider';
-export * from './client-provider';
+export * from './client-provider.dynamic';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
