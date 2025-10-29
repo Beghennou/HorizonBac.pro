7939b210aff4e93b5ff3790971f01a2731235716
Hamed BEGHENNOU <hamed.beghennou@gmail.com>
2025-10-29 18:58:23 +0000

    https://thumbs.dreamstime.com/z/car-repair-icons-black-auto-vehicle-mech

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'thumbs.dreamstime.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
